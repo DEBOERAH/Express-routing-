53650a33aa5ae8355f07ee860eb0f92d1982e6d8
DEBOERAH <eniolaofuani@gmail.com>
2024-11-05 18:43:27 +0100

    initial commit

---
+++ Index.js
@@ -0,0 +1,37 @@
+//import express
+const express = require('express');
+
+//create express appinstance
+const app = express();
+
+//define port
+const port = 3096;
+
+// define index route
+app.get('/', (req, res) => {
+    // send index.html
+    res.sendFile(__dirname + '/index.html');
+});
+// define /austine route
+app.get('/austine', (req, res) => {
+    // send index.html
+    res.sendFile(__dirname + '/austine.html');
+});
+// define /mimi route
+app.get('/mimi', (req, res) => {
+    // send index.html
+    res.sendFile(__dirname + '/mimi.html');
+});
+
+// define /ada route
+app.get('/ada.html', (req, res) => {
+    // send index.html
+    res.sendFile(__dirname + '/ada.html');
+});
+
+
+
+// listen to port 
+app.listen(port, () => {
+    console.log(`Server is running on port http://localhost:${port}`);
+})
